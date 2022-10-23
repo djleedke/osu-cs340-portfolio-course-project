7cbd7672d649a5249a8f038e57a310650b4c7f59
djleedke <leedke@gmail.com>
2022-10-23 14:13:17 -0700

    Updates to index page

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
-from flask import Flask, render_template, json, redirect
+from flask import Flask, render_template
 from flask_mysqldb import MySQL
-from flask import request
-import os, config
+import config
 
 app = Flask(__name__)
 
@@ -15,7 +14,29 @@ mysql = MySQL(app)
 
 @app.route('/')
 def index():
-    return render_template('pages/index.html')
+
+    context = {}
+
+    try:
+        # Getting counts from each table to display the # of rows
+        cursor = mysql.connection.cursor()
+        query = (
+            "SELECT" 
+            "(SELECT COUNT(*) FROM chefs) as chefs,"
+            "(SELECT COUNT(*) FROM dishes) as dishes,"
+            "(SELECT COUNT(*) FROM dish_has_recipe) as dish_has_recipe,"
+            "(SELECT COUNT(*) FROM ingredients) as ingredients,"
+            "(SELECT COUNT(*) FROM recipes) as recipes,"
+            "(SELECT COUNT(*) FROM recipe_has_ingredient) as recipe_has_ingredient,"
+            "(SELECT COUNT(*) FROM restaurants) as restaurants"
+        )
+
+        cursor.execute(query)
+        context = cursor.fetchall()[0]
+    except Exception as e:
+        print(e)
+
+    return render_template('pages/index.html', context=context)
 
 @app.route('/chefs')
 def chefs():
@@ -25,6 +46,10 @@ def chefs():
 def dishes():
     return render_template('pages/dishes.html')
 
+@app.route('/dish-has-recipe')
+def dish_has_recipe():
+    return render_template('pages/dish_has_recipe.html')
+
 @app.route('/ingredients')
 def ingredients():
     return render_template('pages/ingredients.html')
@@ -32,6 +57,10 @@ def ingredients():
 @app.route('/recipes')
 def recipes():
     return render_template('pages/recipes.html')
+
+@app.route('/recipe-has-ingredient')
+def recipe_has_ingredient():
+    return render_template('pages/recipe_has_ingredient.html')
 
 @app.route('/restaurants')
 def restaurants():
